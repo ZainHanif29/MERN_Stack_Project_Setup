6d4a3f69e212e76de7c136cbe1862459f581009c
ZAIN-UL-ABEDIN <zainhanif2902@gmail.com>
2024-10-14 07:09:41 -0700

    client setup
    react project using tailwind css and shad cn

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col min-h-screen min-w-screen items-center justify-center text-center">
+        <h1 className="animate-bounce text-5xl text-gray-800">
+          React App
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
